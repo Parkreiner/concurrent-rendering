57c8a72a19b504f89e918a71e1c13d0e97f97c99
Michael Smith <throwawayclover@gmail.com>
2025-08-21 00:18:37 -0400

    refactor: centralize files

--- app/dumping-ground/Experiments.tsx
+++ app/dumping-ground/Experiments.tsx
@@ -1,9 +1,9 @@
-import { useState, type FC, type ReactNode } from "react";
+import { memo, useState, type FC, type ReactNode } from "react";
 import { Button } from "./Button";
 import { useForcePeriodicRefresh } from "./useForcePeriodicRerender";
 import { DemoToggleButton } from "./DemoToggleButton";
-import { MemoizedCardList, CardList } from "./CardList";
-import type { CardData } from "./mockCards";
+import { filterCards, type CardData } from "./mockCards";
+import { Card } from "./Card";
 
 // Clunky props contract
 type ExperimentsProps = Readonly<{
@@ -133,3 +133,46 @@ export const Experiments: FC<ExperimentsProps> = ({
     </section>
   );
 };
+
+type CardListProps = Readonly<{
+  cards: readonly CardData[];
+  query: string;
+  isThrottled: boolean;
+}>;
+
+export const CardList: FC<CardListProps> = ({ cards, query, isThrottled }) => {
+  return (
+    <section>
+      <h2 className="sr-only">Card List</h2>
+      <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+        {filterCards(cards, query, isThrottled).map((r) => (
+          <li key={r.id}>
+            <Card
+              headerLevel="h3"
+              className="h-full"
+              displayName={r.displayName}
+              authorName={r.author}
+              description={r.description}
+              iconStyle={r.iconStyle}
+              tags={r.tags}
+            />
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+};
+
+export const MemoizedCardList = memo<CardListProps>(
+  (props) => {
+    return <CardList {...props} />;
+  },
+  (oldProps, newProps) => {
+    const skipUpdate =
+      oldProps.cards === newProps.cards &&
+      oldProps.isThrottled === newProps.isThrottled &&
+      oldProps.query === newProps.query;
+    console.log("skip", skipUpdate);
+    return skipUpdate;
+  }
+);
